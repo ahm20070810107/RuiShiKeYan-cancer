15201dc41689a4a7bb14d1bbafa0c341afa66e9b
Hubot <ahm20070810107@163.com>
2018-02-27 16:45:55 +0800

    shell批量执行

---
+++ src/main/resources/runLChuangShenyan_all-13ra.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_QueZzhengLeiJi.ReadLeijiTable
+java -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_QueZzhengLeiJi.PIDIdentity_table
+java -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_QueZzhengLeiJi.ExportShouZhenTable
+java -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar com.RuiShiKeYan.EntityStatistic.ExportExcel2Kt
+java -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar com.RuiShiKeYan.EntityStatistic.ExportCount3Kt
+java -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar com.RuiShiKeYan.RAExportTable.ExportRALeiJiTable
+java -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_QueZzhengLeiJi.ExportLJ_5Tables
+#java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_LangChuang.ExportSlePingFenTable
+#java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_LangChuang.ExportSleLeiJiTable
+#java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_QueZzhengLeiJi.SLELeiJiProbabilityTable
+java -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_QueZzhengLeiJi.ExportWPSEntityStatistic
+java -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_QueZzhengLeiJi.ExportQueZhenTable V0.01 true
+java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar com.RuiShiKeYan.ExportTables.ExportFenZuYYStatus
+java -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar com.RuiShiKeYan.RAExportTable.ExportYYRelatedTable
+#java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_QueZzhengLeiJi.Export_XiTongLeiJiBiao
+java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_QueZzhengLeiJi.Export_ChuFaLeiJiBiao
+#java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar com.RuiShiKeYan.ExportTables.ExportWholePheno_LnPrediction
+#java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_LangChuang.ExportLangChuangShengYanRuZuTable
+#java  -cp /data/dax_backup/current_dax_home/home/睿识科研/huangming/RuiShiKeYan-13RA-1.4-SNAPSHOT.jar test.java.task_SLE_LangChuang.ExportLangCShengYanYinShuBiao
+
