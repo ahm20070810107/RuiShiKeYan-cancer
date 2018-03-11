47d633cfbcd225e2f31ad4280e4b2e1e304b7be9
Hubot <ahm20070810107@163.com>
2018-03-11 23:43:44 +0800

    test

--- src/main/java/com/RuiShiKeYan/RAExportTable/ExportRALeiJiTable.java
+++ src/main/java/com/RuiShiKeYan/RAExportTable/ExportRALeiJiTable.java
@@ -4,6 +4,8 @@ import com.RuiShiKeYan.Common.Interface.IruiShiKeYan;
 import com.RuiShiKeYan.Common.Method.*;
 import com.RuiShiKeYan.SubMethod.getHDPInfo;
 import com.alibaba.fastjson.JSONObject;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.yiyihealth.data.DaX.reader.DSExcelReader2;
 import org.apache.poi.ss.usermodel.Row;
@@ -13,6 +15,7 @@ import test.java.task_SLE_LangChuang.BaseInfo_Title_ListValue_DBCondition;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,12 +30,14 @@ public class ExportRALeiJiTable extends RuiShiKeYan  implements IruiShiKeYan{
 
     Map<String,JSONObject> mapPId=new HashMap<String, JSONObject>();
     private    Map<String,Document> mapYY = new HashMap<String, Document>();
+    private    Map<String,ArrayList<Document>> mapYYAllTime = new HashMap<String, ArrayList<Document>>();
     private    Map<String,Document> mapYYMD = new HashMap<String, Document>();
     private    Map<String,Document> mapZD = new HashMap<String, Document>();
 
     Map<String,ArrayList<String>> mapADISubItem= new HashMap<String, ArrayList<String>>();
     Map<String,ArrayList<String>> mapADRSubItem= new HashMap<String, ArrayList<String>>();
-    Map<String,String> mapSubAndSysMapping=new HashMap<String, String>();
+    Map<String,String> mapISubAndSysMapping=new HashMap<String, String>();
+    Map<String,String> mapRSubAndSysMapping=new HashMap<String, String>();
     final String publicTitle="PID,医院,性别,生产状况分组,地域,首诊时间天,首诊年龄";
     public static void main(String[] args) throws Exception
     {
@@ -75,7 +80,7 @@ public class ExportRALeiJiTable extends RuiShiKeYan  implements IruiShiKeYan{
                 int cell = 0;
                 cell = fillPublicCell(mappid.getKey(), row, jsonBasic, cell);
                 row.createCell(cell++).setCellValue(mapTable.getKey());
-                row.createCell(cell++).setCellValue(mapSubAndSysMapping.get(mapTable.getKey()));
+                row.createCell(cell++).setCellValue(mapISubAndSysMapping.get(mapTable.getKey()));
                 row.createCell(cell++).setCellValue(getJSonValue(jsonObject, "firstTime"));
                 row.createCell(cell++).setCellValue(getJSonValue(jsonObject, "RID"));
             }
@@ -96,39 +101,44 @@ public class ExportRALeiJiTable extends RuiShiKeYan  implements IruiShiKeYan{
 
             // 3/7去掉时间限制
 //            String strShouZhenTime=jsonBasic.getString("诊断时间天");
-            String strSYRIDTime="";//getJSonValue(jsonBasic,"生产状况RID记录时间天");
+         //   String strSYRIDTime="";//getJSonValue(jsonBasic,"生产状况RID记录时间天");
 //            if(strSYRIDTime.equals("") ||strSYRIDTime.compareTo(strShouZhenTime)<= 0 )
 //            {
 //                strSYRIDTime="";
 //            }
             for(Map.Entry<String,ArrayList<String>> mapTable:mapADRSubItem.entrySet()) {
-
-                JSONObject jsonObject = getEntityFirstTime(mappid.getKey(), mapYY, mapTable.getValue(),strSYRIDTime);
-                if (jsonObject == null)  //为空的话就创建空cell
+          //3/9 添加 系统项为"其他药物"的子项不要
+                if(mapRSubAndSysMapping.get(mapTable.getKey())!=null &&mapRSubAndSysMapping.get(mapTable.getKey()).equals("其它药物"))
+                {
+                    continue;
+                }
+                List<Document> listDocuments = getEntityFirstTime(mappid.getKey(), mapYYAllTime, mapTable.getValue());
+                if (listDocuments.size() <1)  //为空的话就创建空cell
                 {
                    continue;
                 }
-                Row row = sheet.createRow(Rownum++);
-                int cell = 0;
-                cell = fillPublicCell(mappid.getKey(), row, jsonBasic, cell);
-                row.createCell(cell++).setCellValue(getJSonValue(jsonBasic,"生产状况RID记录时间天"));
-                row.createCell(cell++).setCellValue(mapTable.getKey());
-                row.createCell(cell++).setCellValue(mapSubAndSysMapping.get(mapTable.getKey()));
-                row.createCell(cell++).setCellValue(getJSonValue(jsonObject, "firstTime"));
-                row.createCell(cell++).setCellValue(getJSonValue(jsonObject, "RID"));
-                try
-                {
-                    String yyYear=getJSonValue(jsonObject, "firstTime").substring(0,4);
-                    String birthYear=getJSonValue(jsonBasic,"出生年");
-                    Integer yyAge=Integer.valueOf(yyYear)-Integer.valueOf(birthYear);
-                    row.createCell(cell++).setCellValue(yyYear);
-                    row.createCell(cell++).setCellValue(birthYear);
-                    row.createCell(cell++).setCellValue(yyAge);
+                for(Document jsonObject:listDocuments) {
+
+                    Row row = sheet.createRow(Rownum++);
+                    int cell = 0;
+                    cell = fillPublicCell(mappid.getKey(), row, jsonBasic, cell);
+                    row.createCell(cell++).setCellValue(getJSonValue(jsonBasic, "生产状况RID记录时间天"));
+                    row.createCell(cell++).setCellValue(mapTable.getKey());
+                    row.createCell(cell++).setCellValue(mapRSubAndSysMapping.get(mapTable.getKey()));
+                    row.createCell(cell++).setCellValue(get10JSonValue(jsonObject, "用药时间"));
+                    row.createCell(cell++).setCellValue(getJSonValue(jsonObject, "RID"));
+                    try {
+                        String yyYear = getJSonValue(jsonObject, "用药时间").substring(0, 4);
+                        String birthYear = getJSonValue(jsonBasic, "出生年");
+                        Integer yyAge = Integer.valueOf(yyYear) - Integer.valueOf(birthYear);
+                        row.createCell(cell++).setCellValue(yyYear);
+                        row.createCell(cell++).setCellValue(birthYear);
+                        row.createCell(cell++).setCellValue(yyAge);
 
 
-                }catch (Exception e)
-                {
-                    e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
@@ -158,7 +168,7 @@ public class ExportRALeiJiTable extends RuiShiKeYan  implements IruiShiKeYan{
                 int cell = 0;
                 cell = fillPublicCell(mappid.getKey(), row, jsonBasic, cell);
                 row.createCell(cell++).setCellValue(mapTable.getKey());
-                row.createCell(cell++).setCellValue(mapSubAndSysMapping.get(mapTable.getKey()));
+                row.createCell(cell++).setCellValue(mapRSubAndSysMapping.get(mapTable.getKey()));
                 row.createCell(cell++).setCellValue(getJSonValue(jsonObject, "firstTime"));
                 row.createCell(cell++).setCellValue(getJSonValue(jsonObject, "RID"));
                 row.createCell(cell++).setCellValue(getJSonValue(jsonObject,"段落标题"));
@@ -167,6 +177,27 @@ public class ExportRALeiJiTable extends RuiShiKeYan  implements IruiShiKeYan{
 
         saveExcelTool.saveExcel("交付/用药组合表.xlsx");
     }
+
+    private List<Document> getEntityFirstTime(String strPId,Map<String,ArrayList<Document>> mapEntity,ArrayList<String> arrayList)
+    {
+        List<Document> arrList= new ArrayList<Document>();
+
+        for (String strTableName:arrayList) {
+            ArrayList<Document> arrDocument=mapEntity.get(strPId+strTableName);
+            if(arrDocument !=null)
+            {
+                for(Document document : arrDocument)
+                {
+                    arrList.add(document);
+                }
+            }
+        }
+
+        return arrList;
+    }
+
+
+
     private JSONObject getEntityFirstTime(String strPId,Map<String,Document> mapEntity,ArrayList<String> arrayList,String strSYRIDTime)
     {
         JSONObject jsonObject= new JSONObject();
@@ -210,14 +241,33 @@ public class ExportRALeiJiTable extends RuiShiKeYan  implements IruiShiKeYan{
     private void getBasicInfo(MongoDatabase mdb) throws Exception
     {
         String fileName = LocalHostInfo.getPath() + "交付/首诊时间表.xlsx";
-    //    String fileBasic = LocalHostInfo.getPath() + "交付/PID验证表.xlsx";
         ReadExcelToMap.readFromExcelToMap(mapPId,fileName,"患者（PID）",true);
         getSubItemMap();
         getHDPInfo.getYYDay(mdb,mapYY,"");
+        getYYAllDay(mdb,mapYYAllTime,"");
         getHDPInfo.getYYDay(mdb,mapYYMD,",'段落标题':{$in:['出院医嘱','出院带药','出院后用药及建议']}");
         getHDPInfo.getZDDay(mdb,mapZD,"");
     }
+    public  void getYYAllDay(MongoDatabase dbHDP,Map<String,ArrayList<Document>> mapYYAll,String strConditon)
+    {
+        System.out.println("getAllYYDay");
+        MongoCollection<Document> mc = dbHDP.getCollection("ADR");
+        ArrayList<Document> aggregates = new ArrayList<Document>();
+        String strZDCondition="{"+BaseInfo_Title_ListValue_DBCondition.ADR13+strConditon+",'用药时间':{$exists:true,$regex:/^.{10,}$/},'是否使用':'使用','通用名':{$ne:'',$exists:true}}";
+        aggregates.add(new Document("$match",Document.parse(strZDCondition)));
+        aggregates.add(new Document("$project",Document.parse("{'用药时间':'$用药时间','通用名':'$通用名','PID':'$PID','RID','$RID'")));//,'段落标题':'$段落标题'
+      //  aggregates.add(new Document("$sort",Document.parse("{'用药时间':1}")));
+        aggregates.add(new Document("$group",Document.parse("{'_id':{'PID':'$PID','通用名':'$通用名'}, 'result':{'$push':'$$ROOT'}}")));
+        MongoCursor<Document> cursor =mc.aggregate(aggregates).allowDiskUse(true).iterator();
+        while (cursor.hasNext())
+        {
+            Document  mcursor =cursor.next();
+            Document groupInfo=(Document)mcursor.get("_id");
 
+            ArrayList arrDocument= (ArrayList)mcursor.get("result");
+            mapYYAll.put(groupInfo.getString("PID")+groupInfo.getString("通用名"),arrDocument);
+        }
+    }
 
     private  void getSubItemMap() throws Exception
     {
@@ -241,7 +291,7 @@ public class ExportRALeiJiTable extends RuiShiKeYan  implements IruiShiKeYan{
                 if(arrayList ==null) {
                     arrayList = new ArrayList<String>();
                     mapADISubItem.put(tempZiXiang,arrayList);
-                    mapSubAndSysMapping.put(tempZiXiang,tempSystem);
+                    mapISubAndSysMapping.put(tempZiXiang,tempSystem);
                 }
                 arrayList.add(getJSonValue(document,"表型名称") +getJSonValue(document,"标准标本"));
             }else if(getJSonValue(document,"表型").equals("用药通用名"))
@@ -250,7 +300,7 @@ public class ExportRALeiJiTable extends RuiShiKeYan  implements IruiShiKeYan{
                 if(arrayList ==null) {
                     arrayList = new ArrayList<String>();
                     mapADRSubItem.put(tempZiXiang,arrayList);
-                    mapSubAndSysMapping.put(tempZiXiang,tempSystem);
+                    mapRSubAndSysMapping.put(tempZiXiang,tempSystem);
                 }
                 arrayList.add(getJSonValue(document,"表型名称") +getJSonValue(document,"标准标本"));
 
